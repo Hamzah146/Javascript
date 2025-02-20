100c76122d4a7b07c765e97cbe34a7312f9cfbdc
Hamzah146 <h4mzah.146@gmail.com>
2025-02-20 09:13:19 +0700

    Add files via upload

---
+++ task_1/task1.js
@@ -0,0 +1,18 @@
+alert("Hello World");
+
+alert("Selamat datang di tutorial belajar Javascript");
+
+function sayHello(){
+    alert("Hello");
+}
+
+// document.write("<h1>Hello World</h1>");
+// document.write("<hr>");
+// document.write("<p>Saya sedang belajar Javascript</p>");
+// document.write("di <b>petanikode.com</b>");
+
+// membuat objek elemen
+var hasil = document.getElementById("hasil-output");
+
+// menampilkan output ke elemen hasil
+hasil.innerHTML = "<p>Aku suka Javascript</p>";
