6219118327df787447b9fc52ee6bf4203ff555ce
Hamzah146 <h4mzah.146@gmail.com>
2025-02-21 13:54:03 +0700

    Add files via upload

---
+++ task_5.js
@@ -0,0 +1,12 @@
+// alert("Selamat datang di tutorial Javascript");
+
+// var yakin = confirm("Apakah kamu yakin akan mengunjungi petanikode?");
+
+// if (yakin) {
+//     window.location = "https://www.petanikode.com";
+// } else {
+//     document.write("Baiklah, tetap di sini saja ya :)");
+// }
+
+var nama = prompt("Siapa nama kamu?", "");
+document.write("<p>Hello "+ nama +"</p>");
